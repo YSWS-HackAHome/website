e91159f19d3c84426e91073d943e1c2facdfbcf3
rawnullbyte <andrexyt2012@gmail.com>
2026-03-29 17:03:50 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -20,21 +20,17 @@ if [ "$skip_dependencies" == true ]; then
 else
   echo "📦 Installing Dependencies..."
   pip install -r requirements.txt
-  cd web
   npm install
-  cd ..
 fi
 
 if [ "$skip_build" == true ]; then
   echo "📦 Skipping Frontend Build..."
 else
   echo "📦 Building Frontend..."
-  cd web
   if [ -d "dist" ]; then
       rm -R dist
   fi
   npm run build
-  cd ..
 fi
 
 echo "🔥 Launching Backend..."
