2fb10482158022537949566c42758e0c38e43b8a
rawnullbyte <120054040+rawnullbyte@users.noreply.github.com>
2026-03-29 18:14:28 +0200

    Update Builders Joining target in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -246,9 +246,9 @@ function Hero() {
                 </div>
 
                 <div className="hero-stats">
-                    <CountStat target={2} label="Builders Joining" />
+                    <CountStat target={10+} label="Builders Joining" />
                     <CountStat target={0} label="Projects submitted" />
-                    <CountStat target={3} label="Week Program" />
+                    {/* <CountStat target={3} label="Week Program" /> - we dont know yet ¯\_(ツ)_/¯ */}
                 </div>
             </div>
 
